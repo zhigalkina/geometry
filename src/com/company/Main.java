f337afb98f66205c0ec199ed93282b7926ba76ec
zhigalkina <39518337+zhigalkina@users.noreply.github.com>
2018-05-31 15:48:51 +1000

    123123423423

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,7 @@ public class Main {
 	// write your code here
         System.out.println ("helowsdqwd");
         System.out.println ("hasdasdasd");
+
+        System.out.println ("hasdasdasd");
     }
 }
